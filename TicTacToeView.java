6ba9a08ae2fec6e74953d76dfc7fda100bce1962
Hubert Dang <hubertdang@gmail.com>
2023-03-31 17:02:03 -0400

    Adjusted view to take information on buttons from the controller, making it the sole observer of the model.

--- TicTacToeView.java
+++ TicTacToeView.java
@@ -15,6 +15,9 @@ public class TicTacToeView extends JPanel implements Observer {
 
     private JTextField gameStateDisplay;
     private JMenuItem quitItem;
+    private static ImageIcon oIcon = new ImageIcon("oicon.jpg");
+    private static ImageIcon xIcon = new ImageIcon("xicon.jpg");
+    private TicTacToeController controller;
 
     public TicTacToeView() {
         gameStateDisplay = new JTextField();
@@ -27,6 +30,18 @@ public class TicTacToeView extends JPanel implements Observer {
 
 
     /**
+     * Set the controller to controller.
+     *
+     * Note: This method was created to be able to access the game's
+     * controller from within the view.
+     *
+     * @param controller
+     */
+    public void setController(TicTacToeController controller) {
+        this.controller = controller;
+    }
+
+    /**
      * See the documentation for java.util.Observer.
      */
     public void update(Observable o, Object arg) {
@@ -34,6 +49,19 @@ public class TicTacToeView extends JPanel implements Observer {
         // display current game state
         String gameState = model.getGameState();
         gameStateDisplay.setText(gameState);
+        // display the correct marks on the buttons
+        String[][] board = model.getBoard();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // set button icons according to their mark
+                if (board[i][j] == "X") {
+                    controller.getButton(i, j).setIcon(xIcon);
+                }
+                if (board[i][j] == "O") {
+                    controller.getButton(i, j).setIcon(oIcon);
+                }
+            }
+        }
     }
 }
 
