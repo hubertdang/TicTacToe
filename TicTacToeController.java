12a60a29881d02b2f0f10b7c1770348150ecbc8e
Hubert Dang <hubertdang@gmail.com>
2023-03-31 18:04:43 -0400

    Add a menu bar with an options menu to allow user to start a new game and quit.

--- TicTacToeController.java
+++ TicTacToeController.java
@@ -15,6 +15,8 @@ import java.util.Observer;
 public class TicTacToeController extends JFrame implements ActionListener {
     private JButton buttonBoard[][]; // 3x3 array of buttons on the board
     private TicTacToeModel model;
+    private JMenuItem newItem;
+    private JMenuItem quitItem;
 
     public TicTacToeController(TicTacToeView view, TicTacToeModel model) {
         super("TicTacToe");
@@ -48,6 +50,22 @@ public class TicTacToeController extends JFrame implements ActionListener {
         }
         // place buttons on top of the game state
         contentPane.add(buttonPanel, BorderLayout.CENTER);
+        // place menu above buttons
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar); // add menu bar to the frame
+
+        JMenu fileMenu = new JMenu("Options"); // create a menu
+        menuBar.add(fileMenu); // and add to the menu bar
+
+        newItem = new JMenuItem("New"); // create a menu item called "New"
+        fileMenu.add(newItem); // and add to the menu
+
+        quitItem = new JMenuItem("Quit"); // create a menu item called "Quit"
+        fileMenu.add(quitItem); // and add to the menu
+
+        final int SHORTCUT_MASK = Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx(); // to save typing
+        newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, SHORTCUT_MASK));
+        quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, SHORTCUT_MASK));
     }
 
 
@@ -60,6 +78,16 @@ public class TicTacToeController extends JFrame implements ActionListener {
                 buttonBoard[i][j].addActionListener(this);
             }
         }
+        // listen for menu selections
+        newItem.addActionListener(this);
+        quitItem.addActionListener(new ActionListener() // create an anonymous inner class
+                                   { // start of anonymous subclass of ActionListener
+                                       // this allows us to put the code for this action here
+                                       public void actionPerformed(ActionEvent event) {
+                                           System.exit(0); // quit
+                                       }
+                                   } // end of anonymous subclass
+        ); // end of addActionListener parameter list and statement
     }
 
 
@@ -70,21 +98,37 @@ public class TicTacToeController extends JFrame implements ActionListener {
      * @param e the event to be processed
      */
     public void actionPerformed(ActionEvent e) {
-        JButton button = (JButton) e.getSource();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (button == buttonBoard[i][j]) {
-                    model.setSquare(i, j);
-                    break;
-                }
-            }
-        }
-        // Disable all buttons when someone has won the game
-        if (model.haveWinner()) {
+        Object o = e.getSource(); // get the action
+
+        if (o instanceof JButton) {
+            JButton button = (JButton) o;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    buttonBoard[i][j].setEnabled(false);
+                    if (button == buttonBoard[i][j]) {
+                        model.setSquare(i, j);
+                        break;
+                    }
                 }
+            }
+            // Disable all buttons when someone has won the game
+            if (model.haveWinner()) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        buttonBoard[i][j].setEnabled(false);
+                    }
+                }
+            }
+        } else { // it's a JMenu item
+            JMenuItem item = (JMenuItem) o;
+
+            if (item == newItem) {
+                // enable all buttons in case user is starting new game after a win
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        buttonBoard[i][j].setEnabled(true);
+                    }
+                }
+                model.newGame();
             }
         }
     }
